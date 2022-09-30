8ca4b9a7c120941f56aca40658d0d2fa3818bd82
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2022-09-30 17:54:21 +0200

    update connect database

--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
 exports.port = process.env.PORT || 4000
-exports.origin = process.env.ORIGIN || `http://localhost:${exports.port}`
+exports.origin = process.env.ORIGIN || `http://demoapp-env.eba-nbxmmkxc.us-east-1.elasticbeanstalk.com/`
