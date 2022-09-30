fb03074294adc7e5175efbba9bf011b1f54ca9c8
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2022-09-30 17:27:31 +0200

    connect database

--- server/contacts.js
+++ server/contacts.js
@@ -1,65 +1,60 @@
-const clone = require('clone')
-const config = require('./config')
+const clone = require("clone");
+const config = require("./config");
+const pg = require("pg");
 
-const db = {}
+const db = {};
 
-const defaultData = {
-  contacts: [
-    {
-      id: '242dfef8-529a-11eb-ae93-0242ac130002',
-      name: 'Ryan Florence',
-      email: 'ryan@reacttraining.com',
-      avatarURL: config.origin + '/ryan.jpg'
-    },
-    {
-      id: '299992c6-529a-11eb-ae93-0242ac130002',
-      name: 'Michael Jackson',
-      email: 'michael@reacttraining.com',
-      avatarURL: config.origin + '/michael.jpg'
-    },
-    {
-      id: '307f0b0c-529a-11eb-ae93-0242ac130002',
-      name: 'Tyler McGinnis',
-      email: 'tyler@reacttraining.com',
-      avatarURL: config.origin + '/tyler.jpg'
-    }
-  ]
-}
+const client = new pg.Pool({
+  host: "database-1.crapprtfui5m.us-east-1.rds.amazonaws.com",
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
+  port: 5432,
+});
+const defaultData = async () => {
+  try {
+    const conn = await client.connect();
+    const sql = "SELECT * FROM contacts";
+    const result = await conn.query(sql);
+    conn.release();
+    return result.rows;
+  } catch (err) {}
+};
 
 const get = (token) => {
-  let data = db[token]
+  let data = db[token];
 
   if (data == null) {
-    data = db[token] = clone(defaultData)
+    data = db[token] = clone(defaultData);
   }
 
-  return data
-}
+  return data;
+};
 
 const add = (token, contact) => {
   if (!contact.id) {
-    contact.id = Math.random().toString(36).substr(-8)
+    contact.id = Math.random().toString(36).substr(-8);
   }
 
-  get(token).contacts.push(contact)
+  get(token).contacts.push(contact);
 
-  return contact
-}
+  return contact;
+};
 
 const remove = (token, id) => {
-  const data = get(token)
-  const contact = data.contacts.find(c => c.id === id)
+  const data = get(token);
+  const contact = data.contacts.find((c) => c.id === id);
 
   if (contact) {
-    data.contacts = data.contacts.filter(c => c !== contact)
+    data.contacts = data.contacts.filter((c) => c !== contact);
   }
 
-  return { contact }
-}
+  return { contact };
+};
 
 module.exports = {
   get,
   add,
   remove,
-  defaultData
-}
+  defaultData,
+};
