b22018efe0de45d2561f123e26d1651dab857206
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2022-09-30 16:56:22 +0200

    config pipeline

--- server/bin/deploy.sh
+++ server/bin/deploy.sh
@@ -1,5 +1,5 @@
 # Deploy to AWS BS
-eb init devtest10 --region us-east-1 --platform node.js
+eb init demo-app --region us-east-1 --platform node.js
 eb list
-eb use Devtest10-env
+eb use Demoapp-env
 eb deploy
